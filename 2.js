1e740bb30b13f78b827ae7c3fc59bf34d3f2ab8e
Kikoego <97868051+Kikoego@users.noreply.github.com>
2022-02-14 12:23:15 +0700

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,5 @@
+let height = 23;
+let weight = 10;
+
+let s = height*weight;
+console.log (`s=${s}`)
---
+++ 3.js
@@ -0,0 +1,6 @@
+let s = 2000000;
+let p = 10;
+let years = 5;
+
+let perepl = s / (p*years);
+console.log (`perepl=${perepl}`)
